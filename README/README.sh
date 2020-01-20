c5d9d994d052886c386f217708ffc90c48d81bdb
wbbrisbane <59631742+wbbrisbane@users.noreply.github.com>
2020-01-20 12:14:39 -0500

    Create README.sh

---
+++ README/README.sh
@@ -0,0 +1,20 @@
+# Assignment 1
+
+- name: Warren Brandon Brisbane
+- student ID: 20608837
+
+## Dependencies
+
+- sys
+- json
+- numpy
+- randint from random
+
+## Running `main.py`
+
+To run `main.py`, use
+
+```sh
+python3 main.py <name>.in <name>.json
+```
+where <name> is the file location for the matching .in and .json files
